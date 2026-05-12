e5d5e5b1c2ba15828a171bcae24f2f2aea82d3f6
LaraJhonatan <larajhonatanv@gmail.com>
2026-05-12 00:05:11 -0500

    cambios

--- src/api/http.js
+++ src/api/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-  baseURL: `${import.meta.env.VITE_API_ARMELLINI_BASE_URL}/api`,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   timeout: 15000,
 })
 
