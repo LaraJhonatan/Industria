61aab6d777e4e6e5e90347fabd20c4fdc53b2f7b
LaraJhonatan <larajhonatanv@gmail.com>
2026-05-11 19:40:19 -0500

    cambios version

---
+++ src/components/dashboard/ConstructionPlaceholder.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="construction-placeholder">
+    <q-icon name="construction" size="48px" color="warning" />
+    <p>En construcción</p>
+  </div>
+</template>
+
+<script setup>
+// placeholder
+</script>
